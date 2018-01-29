b569d6bf134fb9afdb50b1483758274eb896dc8c
Adrian Cuzman <adriancuzman@gmail.com>
2018-01-29 12:41:31 +0200

    use opi instead of rpi

--- octoprint_psucontrol/__init__.py
+++ octoprint_psucontrol/__init__.py
@@ -74,28 +74,14 @@ class PSUControl(octoprint.plugin.StartupPlugin,
                    octoprint.plugin.SimpleApiPlugin):
 
     def __init__(self):
-        print "init psu control"
         try:
-            print "trying to find rpi.gpio"
             global GPIO
-            import RPi.GPIO as GPIO
+            import OPi.GPIO as GPIO
+            import orangepi.pc
+            GPIO.BOARD = orangepi.pc.BOARD
             self._hasGPIO = True
-            print "found rpi.gpio"
         except (ImportError, RuntimeError):
             self._hasGPIO = False
-            print "failed to found rpi.gpio"
-        if not self._hasGPIO:
-            try:
-                print "trying to find OPi.GPIO"
-                import OPi.GPIO as GPIO
-                import orangepi.pc
-                GPIO.BOARD = orangepi.pc.BOARD
-                self._hasGPIO = True
-                print "found Opi.GPIO"
-            except (ImportError, RuntimeError):
-                print "failed to find Opi.GPIO"
-                self._hasGPIO = False
-
         self._pin_to_gpio_rev1 = [-1, -1, -1, 0, -1, 1, -1, 4, 14, -1, 15, 17, 18, 21, -1, 22, 23, -1, 24, 10, -1, 9, 25, 11, 8, -1, 7, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 ]
         self._pin_to_gpio_rev2 = [-1, -1, -1, 2, -1, 3, -1, 4, 14, -1, 15, 17, 18, 27, -1, 22, 23, -1, 24, 10, -1, 9, 25, 11, 8, -1, 7, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 ]
         self._pin_to_gpio_rev3 = [-1, -1, -1, 2, -1, 3, -1, 4, 14, -1, 15, 17, 18, 27, -1, 22, 23, -1, 24, 10, -1, 9, 25, 11, 8, -1, 7, -1, -1, 5, -1, 6, 12, 13, -1, 19, 16, 26, 20, -1, 21 ]
